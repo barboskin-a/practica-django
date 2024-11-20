f1f6b2bda393ae92707fd43773929e6fab05729f
Широких Елизавета Романовна <gr431_shiero@ttit.local>
2024-11-20 12:44:49 +0700

    authentication and lists

--- catalog/admin.py
+++ catalog/admin.py
@@ -31,12 +31,13 @@ class BookAdmin(admin.ModelAdmin):
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('book', 'status', 'borrower', 'due_back', 'id')
     list_filter = ('status', 'due_back')
     fieldsets = (
     (None, {
         'fields': ('book', 'imprint', 'id')
     }),
     ('Availability', {
-        'fields': ('status', 'due_back')
+        'fields': ('status', 'due_back', 'borrower')
     }),
 )
